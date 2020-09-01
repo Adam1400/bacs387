5b4a59958cd0fbf0e9337db42f674e55b4db5281
Adam1400 <42878557+Adam1400@users.noreply.github.com>
2020-09-01 12:30:05 -0600

    Added "Analytics" and "Letters and numbers"

---
+++ Inital Projects/Analytics/Analytics/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analytics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> decimals = new List<double>();
+            decimals.Add(0.5);
+            decimals.Add(0.75);
+            decimals.Add(0.758);
+            decimals.Add(0.35);
+            decimals.Add(0.25);
+            decimals.Add(0.24);
+            decimals.Add(0.8133);
+            decimals.Add(0.723);
+            decimals.Add(0.411236);
+            decimals.Add(0.00045645);
+
+            foreach (double x in decimals)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Average: " + decimals.Average());
+            Console.WriteLine("Minimum: " + decimals.Min());
+            Console.WriteLine("Maximum: " + decimals.Max());
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Inital Projects/Letters and Numbers/Letters and Numbers/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Letters_and_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //OVERHEAD
+            //create letter index
+            char[] Cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            char[] low = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            List<char> CapLetters = Cap.ToList();
+            List<char> letters = low.ToList();
+
+
+            //create fibonacci list
+            List<int> fib = new List<int>();
+            int n1 = 0, n2 = 1, n3, i;   
+            for (i = 2; i < letters.Count(); ++i)    
+            {
+                n3 = n1 + n2;
+                fib.Add(n3);
+                n1 = n2;
+                n2 = n3;
+            }
+            fib.Add(1);//missing a and b
+            fib.Add(0);
+            fib.Sort();//cleanup
+
+
+            //CONSOLE APP
+            Console.WriteLine("Enter a word and see its corrisponding fibonacci sum:");
+            string word = Console.ReadLine();
+            int sum = 0;
+
+            foreach (char x in word) 
+            {
+                if(CapLetters.Contains(x) || letters.Contains(x))
+                {
+                    if (CapLetters.IndexOf(x) == -1)
+                    {
+                        //do nothing
+                    }
+                    else 
+                    {
+                        sum = sum + fib[CapLetters.IndexOf(x)];
+                    }
+
+                    if (letters.IndexOf(x) == -1)
+                    {
+                        //do nothing
+                    }
+                    else 
+                    {
+                        sum = sum + fib[letters.IndexOf(x)];
+                    }
+                }
+                
+            }
+
+            Console.WriteLine("fibonacci sum ==> " + sum);
+
+            Console.ReadKey();
+
+        }
+    }
+}
