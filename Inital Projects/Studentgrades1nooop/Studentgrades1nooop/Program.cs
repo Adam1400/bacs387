7965cdabe9e96f2d9a4b2b53cecc3fcb79da25f8
Adam1400 <42878557+Adam1400@users.noreply.github.com>
2020-09-09 12:44:48 -0600

    update

---
+++ Inital Projects/Studentgrades1nooop/Studentgrades1nooop/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studentgrades1nooop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> name = new List<string>();
+            List<double> grade = new List<double>();
+
+            bool running = true;
+
+            while (running == true) {
+
+                Console.WriteLine("1 ==> enter names");
+                Console.WriteLine("2 ==> display average");
+                Console.WriteLine("3 ==> quit");
+                string input = Console.ReadLine();
+
+
+                //option 1
+                if(input == "1") {
+
+                    Console.WriteLine("enter the number of students");
+                    int num = int.Parse(Console.ReadLine());
+
+                    for (int i = 0; i < num; i++) {
+                        Console.WriteLine("enter student: ");
+                        name.Add(Console.ReadLine());
+
+                        Console.WriteLine("enter student grade: ");
+                        grade.Add(double.Parse(Console.ReadLine()));
+
+                    }      
+                }
+
+                //option 2
+                if (input == "2") {
+                    Console.WriteLine("Average: " + grade.Average());
+                
+                }
+
+                //option 3
+                if(input == "3") {
+                    running = false;
+                    break;
+                    
+                    }
+
+
+
+
+            
+            
+            
+            
+            
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Inital Projects/lettersandnumbers2/lettersandnumbers2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lettersandnumbers2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //overhaead
+            char[] Cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            char[] low = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+            List<char> CapLetters = Cap.ToList();
+            List<char> letters = low.ToList();
+            List<int> fib = new List<int>();
+
+            int n1 = 0, n2 = 1, n3, i;
+            for (i = 2; i < letters.Count(); ++i) //loop starts from 2 because 0 and 1 are already printed    
+            {
+                n3 = n1 + n2;
+                fib.Add(n3);
+                n1 = n2;
+                n2 = n3;
+            }
+            fib.Add(1);
+            fib.Add(0);
+            fib.Sort();
+
+            //this is where the overhad stops
+            Console.WriteLine("enter a word");
+            string input = Console.ReadLine();
+            int sum = 0;
+
+            foreach (char x in input) {
+
+                if (CapLetters.Contains(x) || letters.Contains(x)) {
+
+                    //CAPLETTERS
+                    if (CapLetters.IndexOf(x) == -1)
+                    { //checking if x is not in the list
+                        //do nothing 
+                    }
+                    else {
+                        sum = sum + fib[CapLetters.IndexOf(x)];
+                    }
+
+                    //lowercase
+                    if (letters.IndexOf(x) == -1)
+                    { //checking if x is not in the list
+                        //do nothing 
+                    }
+                    else
+                    {
+                        sum = sum + fib[letters.IndexOf(x)];
+                    }
+                }
+            }
+
+            Console.WriteLine("fibonacci sum ==> " + sum);
+
+            Console.ReadKey();
+        }
+    }
+}
