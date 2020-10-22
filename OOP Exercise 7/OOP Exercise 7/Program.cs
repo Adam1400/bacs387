08eee70edfc68efeac094d935aa514aba5662d51
Adam1400 <42878557+Adam1400@users.noreply.github.com>
2020-10-22 12:43:56 -0600

    update for 7

--- OOP Exercise 7/OOP Exercise 7/Program.cs
+++ OOP Exercise 7/OOP Exercise 7/Program.cs
@@ -1,25 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace OOP_Exercise_6
+namespace OOP_Exercise_7
 {
+
+    // MADE ALMOST ALL PUBLIC PROPERTIES PRIVATE. THIS WAY ONCE THE 
+    // OBJECTS HAVE BEEN SET THE ONLY WAY TO ALTER THE ACCOUNTS 
+    // OR ADD FUNDS IS THROUGH THE BANK.
+    // THESE CHANGES WERE IMPLEMENTED IN ACCOUNT, MEMBERS, AND 
+    // BANK TO KEEP THE OBJECTS FROM BEING TAMPRED WITH
+    // ALL GET METHODS ARE PUBLIC, BUT SETTING PROPERTIES CAN 
+    // ONLY BE ATTAINED THROUGH THE APPROPRIATE METHOD. 
+    // OTHERWISE OTHER METHODS COULD ALTER OBJECTS FOUND OUTSIDE 
+    // OF THEIR OWN CLASS. 
+    //
+    // PROPERTIES SET TO PRIVATE ARE (6):
+    // ACCOUNT -> BALANCE, COUNT
+    // MEMBER -> NAME, ACCOUNTS, COUNT 
+    // BANK -> ACCOUNTS, COUNT
+    //
+    // ANY DATA THAT INVOLVED SENSITIVE INFORMATION SUCH AS BALANCES, 
+    // AND MEMBER INFORMATION HAS BEEN SET PRIVATE
+    // OTHERWISE NON SENSITIVE INFORMATION HAS BEEN KEPT PUBLIC SUCH 
+    // AS BANK NAME, ACCOUNT NAME, AND THE AMMOUNT OF THESE ACCOUNTS
+    // OPENED THROUGH THE BANK
+
     class Account
     {
         private double Balance; //made private
-        private string Name; //made private
+        public string Name;
         private int Count; //made private
 
-        public Account(string name, double balance) 
+        public Account(string name, double balance)
         {
             Balance = balance;
             Name = name;
             Count = 0;
         }
 
-        public double AddFunds(double Funds) 
+        public double AddFunds(double Funds)
         {
             Count++;
             Balance = Balance + Funds;
@@ -33,12 +53,12 @@ namespace OOP_Exercise_6
             return Balance;
         }
 
-        public double CheckBalance() 
+        public double CheckBalance()
         {
             return Balance;
         }
 
-        public int getTransactionCount() 
+        public int getTransactionCount()
         {
             return Count;
         }
@@ -49,22 +69,22 @@ namespace OOP_Exercise_6
         }
     }
 
-    class Member 
+    class Member
     {
-        private string Name; //made private
+        public string Name; 
         private List<Account> Accounts; //made private
-        private int Count; //made private
+        public int Count;
 
-        public Member(string name, List<Account> accounts) 
+        public Member(string name, List<Account> accounts)
         {
             Name = name;
             Accounts = accounts;
             Count = 0;
         }
 
-        public string CloseAccount(string accountName) 
+        public string CloseAccount(string accountName)
         {
-            foreach (Account account in Accounts) 
+            foreach (Account account in Accounts)
             {
                 if (account.getName() == accountName)
                 {
@@ -75,15 +95,15 @@ namespace OOP_Exercise_6
             return "No " + accountName + " Found";
         }
 
-        public int memberAccountCount() 
+        public int memberAccountCount()
         {
             return Accounts.Count();
         }
 
-        public int getTransactionCount() 
+        public int getTransactionCount()
         {
             int Total = 0;
-            foreach (Account account in Accounts) 
+            foreach (Account account in Accounts)
             {
                 Total = Total + account.getTransactionCount();
             }
@@ -91,15 +111,15 @@ namespace OOP_Exercise_6
             return Total;
         }
 
-        public string getName() 
+        public string getName()
         {
             return Name;
         }
 
-        public double totalBalance() 
+        public double totalBalance()
         {
             double Total = 0;
-            foreach (Account account in Accounts) 
+            foreach (Account account in Accounts)
             {
                 Total = Total + account.CheckBalance();
             }
@@ -107,44 +127,44 @@ namespace OOP_Exercise_6
         }
     }
 
-    class Bank 
+    class Bank
     {
         private double Balance; //made private
         private List<Member> Members; //made private
 
-        public Bank(List<Member> members) 
-        { 
+        public Bank(List<Member> members)
+        {
             Members = members;
-           
+
             Balance = 0;
 
-            foreach (Member member in Members) 
+            foreach (Member member in Members)
             {
                 Balance = Balance + member.totalBalance();
             }
-           
+
         }
 
-        public string listAllMembers() 
+        public string listAllMembers()
         {
             string memberList = "Members \n---------------------------- \n";
-            foreach (Member person in Members) 
+            foreach (Member person in Members)
             {
-                memberList = memberList + person.getName() +"\n";
+                memberList = memberList + person.getName() + "\n";
             }
 
             return memberList;
         }
 
         public string getAccountCount()
-        { 
+        {
             int Total = 0;
             foreach (Member person in Members)
             {
                 Total = Total + person.memberAccountCount();
             }
 
-            return "Number of Account: " + Total;   
+            return "Number of Account: " + Total;
         }
 
         public string getTransactionCount()
@@ -158,7 +178,7 @@ namespace OOP_Exercise_6
             return "Number of Transactions: " + Total;
         }
 
-        public double getBankBalance() 
+        public double getBankBalance()
         {
             Balance = 0;
             foreach (Member member in Members)
@@ -208,12 +228,12 @@ namespace OOP_Exercise_6
             Member3Accounts[1].RemoveFunds(2000);
             Member1Accounts[1].AddFunds(12000);
             BankMembers[2].CloseAccount("Investments");
-            Console.WriteLine(BankMembers[1].getName() + " added $1000.00 to => "+Member2Accounts[0].getName());
+            Console.WriteLine(BankMembers[1].getName() + " added $1000.00 to => " + Member2Accounts[0].getName());
             Console.WriteLine(BankMembers[2].getName() + " removed $2000.00 to => " + Member3Accounts[1].getName());
             Console.WriteLine(BankMembers[0].getName() + " added $12,000.00 to => " + Member2Accounts[1].getName());
-            Console.WriteLine(BankMembers[2].getName() + " Closed Investments Account" );
-            
-            
+            Console.WriteLine(BankMembers[2].getName() + " Closed Investments Account");
+
+
             Console.WriteLine("--------------------------");
             Console.WriteLine("ADAMS BANK");
             Console.WriteLine("-------------------------");
